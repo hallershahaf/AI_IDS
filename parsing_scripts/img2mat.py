a6d53b63efb85942e4814164bc7c8656c8535043
Idan Tau <idan.tau@gmail.com>
2020-08-22 14:06:36 +0300

    support tcpdump

--- parsing_scripts/img2mat.py
+++ parsing_scripts/img2mat.py
@@ -1,40 +1,39 @@
 def img2mat(sing_or_mult, bin_or_txt):
-
     """Imports"""
+    import re
     import numpy as np
     import os
     
     """Globals"""
-    mtu = 1514
+    MTU = 1514
     cols = 32
-    rows = int(np.ceil(mtu / cols))
-
+    rows = int(np.ceil(MTU / cols))
     """find all packet files"""
     all_files = os.listdir()
     pack_files = []
-
-    # Init packets
-    packets = []
-
     for f in range(len(all_files)):
         if all_files[f].startswith("packet_"):
-            # pack_files contains the names of all the packet files
-            pack_files.append(all_files[f])
-    images = len(pack_files)
-
+            pack_files.append(all_files[f]) #pack_files contains the names of all the packet files
+    images = len(pack_files) 
     """"create variables from the files"""
-    # Single file mode
+    #single file mode
     if sing_or_mult.lower() == "s":
-        # Always binary file mode
-        packets = np.load(pack_files[0])
-    # Multiple files mode
+        #textual file mode
+        if bin_or_txt.lower() == "t":
+            with open(str(pack_files[0]), 'rb') as parsed:
+                raw_data = str(parsed.read())
+            raw_data = raw_data.split('\n\n')
+        #binary file mode
+        elif bin_or_txt.lower() == "b":
+            packets = np.load(pack_files[0])
+    #multiple files mode
     elif sing_or_mult.lower() == "m":
-        packets = np.empty((images, rows, cols))
-        # Textual file mode
+        packets = np.empty((images,rows,cols))
+        #textual file mode
         if bin_or_txt.lower() == "t":
             for p in range(len(pack_files)):
                 packets[p] = np.loadtxt(pack_files[p], delimiter=',')
-        # Binary file mode
+        #binary file mode
         elif bin_or_txt.lower() == "b":
             for p in range(len(pack_files)):
                 packets[p] = np.load(pack_files[p])
