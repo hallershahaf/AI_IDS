b6f69cf7ea37afa2ddd3bccd7bd78bbdf6d7e208
Idan Tau <idan.tau@gmail.com>
2020-10-16 17:21:40 +0300

    rdp_connect comment

--- parsing_scripts/rdp_connect.py
+++ parsing_scripts/rdp_connect.py
@@ -1,5 +1,5 @@
 import os
-
+#TODO make the code suituble for ubuntu terminal rdp to windows (maybe via metasploit)
 
 def rdp_connect(ip_addr):
     commnd = "mstsc /control /admin /V:" + str(ip_addr)
